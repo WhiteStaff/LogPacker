2b8d1211c8055e078470096e3753eaa2220b760f
Unknown <sashakamensk@mail.ru>
2019-03-25 19:06:16 +0300

    try to do more effective

--- Kontur.LogPacker/Helper.cs
+++ Kontur.LogPacker/Helper.cs
@@ -7,7 +7,7 @@ namespace Kontur.LogPacker
 {
     static class Helper
     {
-        public static bool IsLineCorrect(string line)
+        private static bool IsLineCorrect(string line)
         {
             string pattern = @"^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3} \d{1}[\d| ][\d| ][\d| ][\d| ][\d| ]";
             if (line.Length < 33) return false;
@@ -18,7 +18,7 @@ namespace Kontur.LogPacker
             return false;
         }
 
-        public static string PartOfString(string source, int startindex, int count)
+        private static string PartOfString(string source, int startindex, int count)
         {
             if (source.Length < startindex + count - 1) { return "Error"; }
             else
@@ -33,7 +33,7 @@ namespace Kontur.LogPacker
 
         }
 
-        public static string DateDifference(DateTime dateTime1, DateTime dateTime2)
+        private static string DateDifference(DateTime dateTime1, DateTime dateTime2)
         {
             TimeSpan diff = dateTime1 - dateTime2;
             int days, min, ms;
@@ -43,7 +43,8 @@ namespace Kontur.LogPacker
             string result = days + " " + min + " " + ms;
             return result;
         }
-        public static string DateAsString(string line, DateTime dateTime, out int position, out DateTime dateTime1)
+
+        private static string DateAsString(string line, DateTime dateTime, out int position, out DateTime dateTime1)
         {
             int counter = 0;
             int i = 0;
@@ -84,29 +85,112 @@ namespace Kontur.LogPacker
             return curr.ToString("yyyy-MM-dd HH:mm:ss,fff");
         }
 
-        public static byte[] CreateOptimalByteLine(List<byte> byteList, DateTime dateTime, out DateTime dateTime1)
+        private static string Change(string line, DateTime inputDateTime, ulong inputID, Dictionary<string, string> lvlDict, 
+            out DateTime outputDateTime, out ulong outputID)
+        {
+            bool isReallyCorrect;
+            string myID = "";
+            ulong outIdDiff = 0;            
+            int pos = 0;
+            string strToDict = "";
+            string ChangeId;
+            string newline;
+
+            //parse Data
+            string changedDate = DateDifference(DateTime.ParseExact(PartOfString(line, 0, 23), "yyyy-MM-dd HH:mm:ss,fff", null), inputDateTime);
+            outputDateTime = DateTime.ParseExact(Helper.PartOfString(line, 0, 23), "yyyy-MM-dd HH:mm:ss,fff", null);
+            line = line.Remove(0, 24);
+
+            //parse ID
+            for (int i = 1; i < 20; i++)
+            {
+                myID += line[i];
+                if ((line[i] == ' ') && (line[i - 1] != ' '))
+                {
+                    pos = i;
+                    break;
+                }
+            }
+            isReallyCorrect = ulong.TryParse(myID, out outIdDiff);
+            if (!isReallyCorrect)
+            {
+                outputDateTime = inputDateTime;
+                outputID = inputID;                
+                return null;
+            }
+            outputID = outIdDiff;
+            outIdDiff = outIdDiff - inputID;
+            ChangeId = outIdDiff.ToString();
+            if (pos < 6)
+            {
+                pos = 6;
+            }
+            line = line.Remove(0, pos+1);
+
+            //parse loglvl
+            for (int i = 1; i < line.Length; i++)
+            {
+                strToDict += line[i];
+                if ((line[i] == ' ') && (line[i - 1] != ' '))
+                {
+                    pos = i;
+                    break;
+                }
+            }
+            if (line.Length < 6)
+            {
+                outputDateTime = inputDateTime;
+                outputID = inputID;
+                return null;
+            }
+            if (pos < 5)
+            {
+                pos = 5;
+            }
+            if (!lvlDict.TryGetValue(strToDict, out strToDict))
+            {
+                lvlDict.Add(strToDict, "@" + lvlDict.Count);
+                strToDict = "@" + (lvlDict.Count - 1);
+            }
+            line = line.Remove(0, pos);
+
+            //create newline
+            newline = changedDate + " " + ChangeId + " " + strToDict + line;           
+
+            return newline;
+
+        }
+
+        public static byte[] CreateOptimalByteLine(List<byte> byteList, DateTime inputDateTime, ulong currId, Dictionary<string, string> loglvl, 
+            out DateTime outputDateTime, out ulong outID)
         {
             string newline;
             
             string line = System.Text.Encoding.UTF8.GetString(byteList.ToArray());
             if (Helper.IsLineCorrect(line))
             {
-                if (dateTime.Year != 1)
-                {
-                    newline = Helper.DateDifference(DateTime.ParseExact(Helper.PartOfString(line, 0, 23), "yyyy-MM-dd HH:mm:ss,fff", null), dateTime) + line.Remove(0, 23);
+                if (inputDateTime.Year != 1)
+                {                    
+                    newline = Change(line, inputDateTime, currId, loglvl, out outputDateTime, out outID);
+                    if (String.IsNullOrEmpty(newline))
+                    {
+                        byteList.Insert(0, 33);
+                        outputDateTime = inputDateTime;
+                        return byteList.ToArray();
+                    }
                 }
                 else
                 {
                     newline = line;
                 }
 
-                dateTime1 = DateTime.ParseExact(Helper.PartOfString(line, 0, 23), "yyyy-MM-dd HH:mm:ss,fff", null);
+                outputDateTime = DateTime.ParseExact(Helper.PartOfString(line, 0, 23), "yyyy-MM-dd HH:mm:ss,fff", null);
                 
             }
             else
             {
                 byteList.Insert(0, 33);
-                dateTime1 = dateTime;
+                outputDateTime = inputDateTime;
                 return byteList.ToArray();
             }
 
@@ -122,6 +206,7 @@ namespace Kontur.LogPacker
             string line = System.Text.Encoding.UTF8.GetString(byteList.ToArray());            
                 if (dateTime.Year != 1)
                 {
+                //add work with id and log lvl
                     newline = Helper.DateAsString(line, dateTime, out int pos, out dateTime) + line.Remove(0, pos - 1);
                     dateTime1 = dateTime;
                 }
